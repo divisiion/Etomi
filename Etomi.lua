3fb257a2a012e0fa52a5cdb880396aca3b6fc64d
divisiion <62077972+divisiion@users.noreply.github.com>
2022-08-12 00:39:40 -0400

    Create Etomi.lua

---
+++ Etomi.lua
@@ -0,0 +1,22 @@
+--[[
+    Etomi developed by BrownZombie and nate.
+]]--
+
+local settings = {
+    -- init local settings
+}
+
+-- set global settings
+getgenv().settings = settings
+
+-- set up loop
+function main()
+    
+end
+
+-- gui
+function createGUI()
+    -- TODO: orion library code here
+end
+
+-- hooks
